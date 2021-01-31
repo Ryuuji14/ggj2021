2a6764d8e258c3307515820d4dd1c495604eec36
Dany Karam <danykaram97@gmail.com>
2021-01-31 18:46:17 -0400

    update3

---
+++ Assets/Fragment.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fragment : MonoBehaviour
+{
+
+    public static int fragmentsCount = 0;
+  //  public AudioSource fragmentsource;
+  //  public AudioClip clipfragment;
+   // public AudioClip clipfirework;
+
+    // Use this for initialization
+    void Start()
+    {
+        Fragment.fragmentsCount++;
+        //Debug.Log ("hola");
+       // fragmentsource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+           // AudioSource.PlayClipAtPoint(clipfragment, transform.position);
+            Debug.Log("Te faltan por recolectar " + (fragmentsCount - 1).ToString() + " fragmentos.");
+            Destroy(gameObject);
+        }
+    }
+    void OnDestroy()
+    {
+        Fragment.fragmentsCount--;
+        if (Fragment.fragmentsCount <= 0)
+        {
+            GameObject timer = GameObject.Find("GameTimer");
+
+
+
+            Debug.Log("Ganaste!! :)");
+            SceneManager.LoadScene(2);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            /*GameObject[] fireworks = GameObject.FindGameObjectsWithTag ("Firework");
+			foreach (GameObject firework in fireworks) {
+				firework.GetComponent<ParticleSystem> ().Play ();
+			}*/
+            Destroy(timer);
+        }
+
+    }
+
+}
---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextScene();
+        }
+	}
+
+    public void LoadNextScene()
+    {
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
